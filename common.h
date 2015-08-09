3f1818c2d869b3209787c207af48cfaa199ea03e
jiachuang <690796319@qq.com>
2015-08-09 13:18:01 +0800

    common.h

--- common.h
+++ common.h
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 #ifndef _COMMON_H_
 #define _COMMON_H_
 
 void myitoa(int i, char*string);
 
 #endif
-=======
-#ifndef _COMMON_H_
-#define _COMMON_H_
-
-void itoa(int i, char*string);
-
-#endif
->>>>>>> dev
